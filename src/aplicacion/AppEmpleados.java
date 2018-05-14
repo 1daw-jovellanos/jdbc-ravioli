f2116abe0a99db49d859d3998a9e4858e524d864
vjfernandez <vjfernandez@users.noreply.github.com>
2018-05-14 20:08:57 +0200

    initial

--- src/aplicacion/AppEmpleados.java
+++ src/aplicacion/AppEmpleados.java
@@ -8,117 +8,19 @@ public class AppEmpleados {
     Scanner in;
 
     public AppEmpleados() {
-        in = new Scanner(System.in);
-        try {
-            Class.forName("org.h2.Driver");
-        } catch (ClassNotFoundException ex) {
-            System.err.println("Oh cielos, no se que puñetas pasa con el Driver");
-        }
     }
 
     void menuAnnadirEmpleado() {
-        try (Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "")) {
-            System.out.print("Nombre: ");
-            String nombre = in.nextLine();
-            System.out.print("Apellido 1: ");
-            String ape1 = in.nextLine();
-            System.out.print("Apellido 2: ");
-            String ape2 = in.nextLine();
-            System.out.print("Ciudad: ");
-            String ciudad = in.nextLine();
-            System.out.print("Salario: ");
-            double salario = Double.parseDouble(in.nextLine());
-            String sql
-                    = "INSERT INTO empleados(nombre, apellido1, apellido2, ciudad, salario) "
-                    + "VALUES (?, ?, ?, ?, ?)";
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-
-                stmt.setString(1, nombre);
-                stmt.setString(2, ape1);
-                stmt.setString(3, ape2);
-                stmt.setString(4, ciudad);
-                stmt.setDouble(5, salario);
-
-                int cantidadFilas = stmt.executeUpdate();
-                System.out.format("Filas afectadas %d%n", cantidadFilas);
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
     }
 
     void menuListarEmpleados() {
-        try (Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "")) {
-
-            String sql = "SELECT nombre, apellido1, apellido2, ciudad, salario FROM empleados ";
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                try (ResultSet rs = stmt.executeQuery()) {// CABECERA
-                    System.out.format("%-15s%-15s%-15s%-20s%8s%n", "NOMBRE", "APELLIDO1",
-                            "APELLIDO2", "CIUDAD", "SALARIO");
-                    // Línea de guiones
-                    for (int i = 0; i < (15 + 15 + 15 + 20 + 8); i++) {
-                        System.out.print('-');
-                    }
-                    System.out.println();
-                    // Imprimir Datos del resultSet tabulados
-                    while (rs.next()) {
-                        String nombre = rs.getString("nombre");
-                        String ape1 = rs.getString("apellido1");
-                        String ape2 = rs.getString("apellido2");
-                        String ciudad = rs.getString("ciudad");
-                        double salario = rs.getDouble("salario");
-                        System.out.format("%-15s%-15s%-15s%-20s%8.2f%n", nombre, ape1,
-                                ape2, ciudad, salario);
-                    }
-                }
-            }
-            // Línea en blanco, para separar un poco
-            System.out.println();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
     }
 
     void menuBorrarEmpleados() {
-        String cadenaBusqueda;
-        try (Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "")) {
-            try (PreparedStatement stmt = conn.prepareStatement(
-                    "SELECT count(*) cuenta FROM empleados "
-                    + "WHERE lower(apellido1) LIKE ? OR lower(apellido2) LIKE ?"
-            )) {
-                System.out.print("Cadena de búsqueda: ");
-                cadenaBusqueda = in.nextLine();
-                stmt.setString(1, ("%" + cadenaBusqueda + "%").toLowerCase());
-                stmt.setString(2, ("%" + cadenaBusqueda + "%").toLowerCase());
-                try (ResultSet rs = stmt.executeQuery()) {
-                    rs.next();
-                    int cantidadEmpleados = rs.getInt("cuenta");
-                    System.out.format("Se van a borrar %d empleados. ¿Estás seguro [s/N]?: ",
-                            cantidadEmpleados);
-                }
-            }
-
-            String respuesta = in.nextLine();
-            if (respuesta.toLowerCase().equals("s")) {
-                // borrar de verdad
-                try (PreparedStatement stmt = conn.prepareStatement(
-                        "DELETE FROM empleados "
-                        + "WHERE lower(apellido1) LIKE ? OR lower(apellido2) LIKE ?"
-                )) {
-                    stmt.setString(1, ("%" + cadenaBusqueda + "%").toLowerCase());
-                    stmt.setString(2, ("%" + cadenaBusqueda + "%").toLowerCase());
-                    int cantidadFilasAfectadas = stmt.executeUpdate();
-                    System.out.format("Se borraron %d empleados. %n", cantidadFilasAfectadas);
-                }
-            } else {
-                System.out.format("Operación cancelada.%n");
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+       
     }
 
-    void run() throws SQLException {
+    void run() {
         int opcion;
         do {
             System.out.format(
@@ -143,7 +45,7 @@ public class AppEmpleados {
         } while (opcion != 0);
     }
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args){
         new AppEmpleados().run();
     }
 
---
+++ src/modelo/ConexionBd.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConexionBd {
+
+    private static String URL = "jdbc:h2:tcp://localhost/./bdravioli";
+    private static String USER = "sa";
+    private static String PASS = "";
+    private static String CREATION_STATEMENT
+            = "CREATE TABLE empleados("
+            + "id int AUTO_INCREMENT PRIMARY KEY,"
+            + "nombre varchar(35),"
+            + "apellido1 varchar(35),"
+            + "apellido2 varchar(35),"
+            + "ciudad varchar(30),"
+            + "salario double"
+            + ")";
+    private Connection conn;
+
+    static {
+        inicializar();
+    }
+
+    private static void inicializar() {
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Error grave: El Driver de h2 no está disponible");
+        }
+
+        boolean bdExiste = true;
+        try {
+            DriverManager.getConnection(URL + ";IFEXISTS=TRUE", USER, PASS).close();
+        } catch (SQLException ex) {
+            bdExiste = false;
+        }
+
+        if (!bdExiste) {
+            try {
+                Connection conn = DriverManager.getConnection(URL, USER, PASS);
+                conn.prepareStatement(CREATION_STATEMENT).executeUpdate();
+            } catch (SQLException ex) {
+                System.err.println("Error grave: no se pudo crear la tabla");
+            }
+        }
+    }
+
+    public ConexionBd() throws SQLException {
+        conn = DriverManager.getConnection(
+                URL, USER, PASS
+        );
+    }
+
+    public void close() {
+        try {
+            conn.close();
+        } catch (Exception ex) {
+        };
+    }
+}
