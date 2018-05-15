c1614bbf32c32cab0255e0e9fb45ea0b966f39ec
vjfernandez <vjfernandez@users.noreply.github.com>
2018-05-15 19:31:46 +0200

    initial

--- src/modelo/Empleado.java
+++ src/modelo/Empleado.java
@@ -74,7 +74,28 @@ public class Empleado {
     public boolean create() {
         boolean ok = true; // Supongo que la operación va a ir ok;
         try (Connection conn = ConexionBd.obtener()) {
- 
+            String sql = "INSERT INTO empleados (nombre, apellido1, apellido2, "
+                    + "ciudad, salario) VALUES(?, ?, ?, ?, ?)";
+            
+            // En una tabla con PK autoincrementada, podemos solicitar al SGBD que nos
+            // devuelva también la PK que ha asignado. Lo indicamos con la constante
+            // PreparedStatement.RETURN_GENERATED_KEYS como segundo parámetro
+            try (PreparedStatement stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+                stmt.setString(1, getNombre());
+                stmt.setString(2, getApellido1());
+                stmt.setString(3, getApellido2());
+                stmt.setString(4, getCiudad());
+                stmt.setDouble(5, getSalario());
+                stmt.executeUpdate();
+                
+                // El valor asignado a la PK vendrá en un resultset de un solo valor entero
+                // que devuelve el método getGeneratedKeys() del PreparedStatement
+                try (ResultSet rs = stmt.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        setId(rs.getInt(1));
+                    }
+                }
+            }
         } catch (SQLException ex) {
             System.err.println("Método create: Error al insertar/" + ex.getMessage());
         }
